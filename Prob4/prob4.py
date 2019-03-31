ac3915be0c29cc22edffd602897925f8cb2bbd0d
Wir3dToast <edbeta12@gmail.com>
2019-03-31 00:15:50 -0400

    Solved Prob40

--- Prob4/prob4.py
+++ Prob4/prob4.py
@@ -3,10 +3,10 @@ def main():
 
     for factor1 in range(999,99,-1):
         for factor2 in range(factor1,99,-1):
-            product = str(factor1 * factor2)
+            productstr = str(factor1 * factor2)
 
-            if product == product[::-1]:
-                print(product)
+            if productstr == productstr[::-1]:
+                print(productstr + " " + str((factor1,factor2)))
                 return
 
 
--- Prob40/prob40.py
+++ Prob40/prob40.py
@@ -1,5 +1,28 @@
 def main():
-    
 
-if __name__ = "__main__":
+    accum = 1
+    for i in range(0,7):
+        accum *= generateConstant(10**i)
+    print(accum)
+def generateConstant(index):
+    modBase = 10
+    currIndex = 1
+    offset = 1
+    currNum = 1
+
+    while(1):
+
+        if((currNum / modBase) == 1):
+            modBase *= 10 #increment base by one digit
+            offset += 1
+
+        if(currIndex >= index):
+            return int((str(currNum)[currIndex-index]))
+
+
+        currNum += 1
+        currIndex += offset
+
+
+if __name__ == "__main__":
     main()
