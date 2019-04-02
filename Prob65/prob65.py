124fbd619d36d99aae644c6faefe0205dcb7e2d5
Edward Kim <edbeta12@gmail.com>
2019-04-02 13:37:14 -0400

    Solved Problem 65

---
+++ Prob65/prob65.py
@@ -0,0 +1,38 @@
+
+def main():
+
+    #Fill list of constants
+    constantList = [2]
+
+    seqGen = genSequence()
+    for _ in range(0,98): #Shift back by one to account for trivial first term
+        constantList = constantList + [next(seqGen)]
+
+    print(constantList)
+
+    numerator = 1
+    denominator = 1
+
+    for leftInt in constantList[::-1]:
+        numerator,denominator = denominator,numerator
+        numerator = denominator * leftInt + numerator #swap
+
+
+
+    denoTup = tuple(str(numerator))
+
+    accum = 0
+    for i in denoTup:
+        accum += int(i)
+    print(accum)
+
+def genSequence():
+    i = 1
+    while True:
+        yield 1
+        yield 2 * i
+        yield 1
+        i += 1
+
+if __name__ == "__main__":
+    main()
