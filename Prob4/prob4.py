f877e56178a01b3cfb4d41f86f45b9d507f760df
Edward Kim <edbeta12@gmail.com>
2019-07-20 22:49:53 -0400

    Various solutions and starts

--- Prob4/prob4.py
+++ Prob4/prob4.py
@@ -1,14 +1,13 @@
 
 def main():
-
+    cand = set()
     for factor1 in range(999,99,-1):
         for factor2 in range(factor1,99,-1):
             productstr = str(factor1 * factor2)
 
             if productstr == productstr[::-1]:
-                print(productstr + " " + str((factor1,factor2)))
-                return
+                cand.add(int(productstr))
 
-
+    print(max(cand))
 if __name__ == "__main__":
     main()
